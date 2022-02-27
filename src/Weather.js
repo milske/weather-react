3f09f4a74678d3c6656808a6c4ef6f2238f4132e
milske <millanerg@gmail.com>
2022-02-27 16:26:46 +0100

    Improved styling

--- src/Weather.js
+++ src/Weather.js
@@ -56,7 +56,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="Search"
-                className="form-control btn btn-primary shadow-sm"
+                className="form-control btn btn-light shadow-sm"
               />
             </div>
           </div>
